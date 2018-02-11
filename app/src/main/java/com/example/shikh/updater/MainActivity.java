65be4b49c5afca8964b0be46f29c50471ccbb127
Shikhar Tyagi <shikhart98@gmail.com>
2018-02-11 16:08:49 +0530

    some changes
    Signed-off-by: Shikhar Tyagi <shikhart98@gmail.com>

--- app/src/main/java/com/example/shikh/updater/MainActivity.java
+++ app/src/main/java/com/example/shikh/updater/MainActivity.java
@@ -1,14 +1,14 @@
 package com.example.shikh.updater;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.EditText;
+
+import com.melnykov.fab.FloatingActionButton;
 
 public class MainActivity extends AppCompatActivity {
 
