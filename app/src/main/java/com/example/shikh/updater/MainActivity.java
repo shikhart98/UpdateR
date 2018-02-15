ab78826753c33f71b00b10e51de44e755c87da41
Shikhar Tyagi <shikhart98@gmail.com>
2018-02-16 01:58:03 +0530

    Added logout icon
    Signed-off-by: Shikhar Tyagi <shikhart98@gmail.com>

--- app/src/main/java/com/example/shikh/updater/MainActivity.java
+++ app/src/main/java/com/example/shikh/updater/MainActivity.java
@@ -8,7 +8,10 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.SearchEvent;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
+
 import com.example.shikh.updater.model.Session;
 import com.melnykov.fab.FloatingActionButton;
 
@@ -16,12 +19,14 @@ public class MainActivity extends AppCompatActivity {
 
     EditText et_main;
     Session session;
+    ImageButton logouticon;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         session = new Session(this);
+        logouticon = findViewById(R.id.logout_main);
         if(!session.loggedin()){
             logout();
         }
@@ -31,6 +36,12 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 Snackbar.make(view, "Task Added!", Snackbar.LENGTH_SHORT)
                         .setAction("Action", null).show();
+            }
+        });
+        logouticon.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                logout();
             }
         });
     }
