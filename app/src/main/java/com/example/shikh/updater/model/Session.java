0e306af9263e06b28a41ffd716a68dca70e8bfc5
Shikhar Tyagi <shikhart98@gmail.com>
2018-02-15 08:07:26 +0530

    indented and formated some files
    Signed-off-by: Shikhar Tyagi <shikhart98@gmail.com>

--- app/src/main/java/com/example/shikh/updater/model/Session.java
+++ app/src/main/java/com/example/shikh/updater/model/Session.java
@@ -12,18 +12,18 @@ public class Session {
     SharedPreferences.Editor editor;
     Context ctx;
 
-    public Session(Context ctx){
+    public Session(Context ctx) {
         this.ctx = ctx;
         prefs = ctx.getSharedPreferences("myapp", Context.MODE_PRIVATE);
         editor = prefs.edit();
     }
 
-    public void setLoggedin(boolean logggedin){
-        editor.putBoolean("loggedInmode",logggedin);
+    public void setLoggedin(boolean logggedin) {
+        editor.putBoolean("loggedInmode", logggedin);
         editor.commit();
     }
 
-    public boolean loggedin(){
+    public boolean loggedin() {
         return prefs.getBoolean("loggedInmode", false);
     }
 }
