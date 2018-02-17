94255cc7b348b6a613045140c2475bae8f456797
Shikhar Tyagi <shikhart98@gmail.com>
2018-02-17 08:56:04 +0530

    modified model
    Signed-off-by: Shikhar Tyagi <shikhart98@gmail.com>

--- app/src/main/java/com/example/shikh/updater/model/task.kt
+++ app/src/main/java/com/example/shikh/updater/model/task.kt
@@ -4,6 +4,7 @@ package com.example.shikh.updater.model
  * Created by shikh on 11-02-2018.
  */
 data class task(
+        val task_userid: String,
         val task_name: String,
         val task_done: Boolean,
         val task_date: String
